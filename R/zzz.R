58784e80b5ee75b5fd3ad4e0797497ca2fe2a8ae
Dingyin Sun <dingyinsun@dyn-49-127-1-222.its.monash.edu.au>
2025-02-18 14:40:18 +1100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,16 @@
     "dplyr", "doParallel", "foreach", "rlang"
   )
 
+  missing_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+
+  if (length(missing_packages)) {
+    install.packages(missing_packages, dependencies = TRUE, lib = .libPaths()[1])
+  }
+
+  # Load packages quietly without attaching them to the search path
   invisible(lapply(required_packages, function(pkg) {
-    suppressPackageStartupMessages(requireNamespace(pkg, quietly = TRUE))
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+      stop(paste("Package", pkg, "is required but could not be loaded."))
+    }
   }))
 }
