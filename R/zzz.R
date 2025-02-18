267b79d29baaca8e814d0cc2db3320a41f4adb1f
Dingyin Sun <dingyinsun@dyn-49-127-1-222.its.monash.edu.au>
2025-02-18 14:58:46 +1100

    # Load all required packages without attaching them to the search path

--- R/zzz.R
+++ R/zzz.R
@@ -2,21 +2,18 @@
   required_packages <- c(
     "adabag", "caret", "data.table", "e1071", "glmnet", "iml",
     "kernelshap", "neuralnet", "pROC", "randomForest", "xgboost",
-    "dplyr", "doParallel", "foreach", "rlang"
+    "dplyr", "doParallel", "foreach", "rlang", "parallel"
   )
 
+  # Install missing packages
   missing_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
-
-  if (length(missing_packages)) {
+  if (length(missing_packages) > 0) {
     install.packages(missing_packages, dependencies = TRUE, lib = .libPaths()[1])
   }
 
-  # Ensure parallel is loaded
-  if (!"parallel" %in% loadedNamespaces()) {
-    library(parallel)
-  }
-
+  # Load all required packages without attaching them to the search path
   invisible(lapply(required_packages, function(pkg) {
-    requireNamespace(pkg, quietly = TRUE)
+    suppressPackageStartupMessages(library(pkg, character.only = TRUE))
   }))
+
 }
